3bc09b0b88a29096fbf0dbbd2d1cd2d1bf69744f
Dave Green <david.green@tookitaway.co.uk>
2015-02-10 00:06:49 +0000

    Update manifest and manifest builder.

--- Build-Manifest.ps1
+++ Build-Manifest.ps1
@@ -1,1 +1,10 @@
+<#
+    Name  : Build-Manifest (ShadowGroupSync)
+    Author: David Green
+
+    http://www.tookitaway.co.uk/
+    https://github.com/davegreen/shadowGroupSync.git
+	
+#>
+
 New-ModuleManifest -Path "$PSScriptRoot\ShadowGroupSync.psd1" -Author "David Green" -RequiredModules @("ActiveDirectory") -Copyright "2015" -ScriptsToProcess @('Get-ShadowGroup.ps1', 'New-ShadowGroup.ps1', 'Remove-ShadowGroup.ps1', 'Update-ShadowGroup.ps1') -FunctionsToExport @('Get-ShadowGroup', 'New-ShadowGroup', 'Remove-ShadowGroup', 'Update-ShadowGroup') -CmdletsToExport @('Get-ShadowGroup', 'New-ShadowGroup', 'Remove-ShadowGroup', 'Update-ShadowGroup') -ModuleVersion "0.1"
