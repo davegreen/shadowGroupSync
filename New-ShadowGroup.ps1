e814b38b10d36980041c1dcfd227ae4d43316405
Dave Green <david.green@tookitaway.co.uk>
2015-02-12 16:53:39 +0000

    Oops, fix mixup.

--- New-ShadowGroup.ps1
+++ New-ShadowGroup.ps1
@@ -2,9 +2,8 @@ Function New-ShadowGroup
 {
 	[CmdletBinding()]
 	param (
-		[parameter(ValueFromPipeline=$true,
-		ValueFromPipelineByPropertyName=$true)]
-		[Microsoft.ActiveDirectory.Management.ADGroup]$Identity
+		[parameter()]
+		[string]$Name
 	)
 	
 	BEGIN{}
@@ -15,34 +14,14 @@ Function New-ShadowGroup
 
 	.SYNOPSIS 
 	
-	Gets one or more Active Directory shadow groups.
+	Creates an Active Directory shadow group.
 	
 	.DESCRIPTION 
 	
-	The Get-ShadowGroup cmdlet gets a shadow group or performs a search to retrieve multiple 
-	shadow groups from an Active Directory.
+	The New-ADGroup cmdlet creates a new Active Directory group object. Many object properties 
+	are defined by setting cmdlet parameters.
 	
-	The Identity parameter specifies the shadow group to get. This is done in the same 
-	way as the Get-ADGroup cmdlet in the Microsoft ActiveDirectory module. 
-	
-	You can identify a group by its distinguished name (DN), GUID, security identifier (SID), 
-	Security Accounts Manager (SAM) account name, or canonical name. You can also specify group 
-	object variable, such as $<localGroupObject>. This method of getting a group is identical to 
-	the Get-ADGroup cmdlet in the Microsoft ActiveDirectory module, as this is a prerequisite.
-	
-	.PARAMETER  -Identity <ADGroup>
-	
-	Specifies an Active Directory group object by providing one of the following values. 
-	The identifier in parentheses is the LDAP display name for the attribute.
-
-	Distinguished Name (distinguishedName)
-		Example: CN=examplegroup,OU=groups,DC=contoso,DC=com
-	GUID (objectGUID)
-		Example: 88fb5b65-6a57-45ca-9803-87a48cfe7352
-	Security Identifier (objectSid)
-		Example: S-1-5-21-450053616-922836960-1990678075-13371
-	Security Accounts Manager (SAM) Account Name (sAMAccountName)
-		Example: examplegroup
+	.PARAMETER  <Parameter-Name>
 	
 	.EXAMPLE
 	
